[FIX] mixed precision

DataLoader.py
@@ -11,7 +11,7 @@ class DataLoader(object):
     def __init__(
             self, src_word2idx, tgt_word2idx,
             src_insts=None, tgt_insts=None, src_lang_insts=None, tgt_lang_insts=None,
-            fp16=False, cuda=True, batch_size=64, shuffle=True,
+            cuda=True, batch_size=64, shuffle=True,
             is_train=True, sort_by_length=False,
             maxibatch_size=20):
 
@@ -22,7 +22,6 @@ class DataLoader(object):
             assert len(src_insts) == len(tgt_insts)
 
         self.nb_examples = len(src_insts)
-        self.fp16 = fp16
         self.cuda = cuda
         self._n_batch = int(np.ceil(len(src_insts) / batch_size))
 
@@ -166,9 +165,7 @@ class DataLoader(object):
             if self.cuda:
                 inst_data_tensor = inst_data_tensor.cuda()
                 inst_position_tensor = inst_position_tensor.cuda()
-                if self.fp16:
-                    inst_data_tensor = inst_data_tensor.half()
-                    inst_position_tensor = inst_position_tensor.half()
+                
             return inst_data_tensor, inst_position_tensor
 
         if self._iter_count < self._n_batch:

NMTmodelRNN/Models.py
@@ -247,7 +247,7 @@ class EncoderFast(nn.Module):
         self.drop = nn.Dropout(p=dropout)
         self.n_layers = n_layers
         self.d_model = d_model
-        self.cuda = cuda
+        self.cuda_ = cuda
         self.fp16 = fp16
         self.tt = torch.cuda if cuda else torch
 
@@ -258,7 +258,7 @@ class EncoderFast(nn.Module):
 
         h_0 = Variable( self.tt.FloatTensor(self.n_layers * 2, \
                                             batch_size, self.d_model).zero_() )
-        if self.cuda and self.fp16:
+        if self.cuda_ and self.fp16:
             h_0 = h_0.half()
 
         x_in_emb = self.emb(x_in) # (batch_size, x_seq_len, D_emb)
@@ -455,7 +455,7 @@ class Decoder(nn.Module):
         self.n_tgt_vocab = n_tgt_vocab
         self.d_word_vec = d_word_vec
         self.n_max_seq = n_max_seq
-        self.cuda = cuda
+        self.cuda_ = cuda
         self.fp16 = fp16
 
     def forward(self, h_in, h_in_len, y_in, l_in=None, src_lang_oneHot=None, tgt_lang_oneHot=None):
@@ -468,10 +468,9 @@ class Decoder(nn.Module):
         xmask = xlen_to_mask_rnn(h_in_len.tolist(), self.tt) # (batch_size, x_seq_len)
 
         s_0_ = torch.sum(h_in, 1) # (batch_size, D_hid_enc * num_dir_enc)
-        s_0_ = torch.div( s_0_, Variable(self.tt.FloatTensor(h_in_len.tolist()).view(-1,1)) )
-        if self.cuda and self.fp16:
-            s_0_ = s_0_.half()
-
+        tmp = Variable( self.tt.FloatTensor(h_in_len.tolist()).view(-1,1) )
+        tmp = tmp.half() if self.cuda_ and self.fp16 else tmp
+        s_0_ = torch.div( s_0_, tmp )
         s_0 = self.ctx_to_s0(s_0_)
         s_0 = F.tanh(s_0)
         s_tm1 = s_0 # (batch_size, n_layers * d_model)
@@ -558,7 +557,7 @@ class Decoder(nn.Module):
 
         s_tm1 = torch.sum(h_in, 1) # (batch_size, d_ctx)
         s_tm1 = torch.div( s_tm1, Variable(self.tt.FloatTensor(h_in_len).view(batch_size, 1)) )
-        if self.cuda and self.fp16:
+        if self.cuda_ and self.fp16:
             s_tm1 = s_tm1.half()
         s_tm1 = self.ctx_to_s0(s_tm1)
         s_tm1 = F.tanh(s_tm1)
@@ -678,7 +677,7 @@ class NMTmodelRNN(nn.Module):
             fp16=False, cuda=False):
 
         self.fp16 = fp16
-        self.cuda = cuda
+        self.cuda_ = cuda
 
         self.n_layers = n_layers
         self.uni_steps = uni_steps
@@ -736,7 +735,7 @@ class NMTmodelRNN(nn.Module):
 
     def lang2oneHot(self, lang_seq, langIdx2oneHotIdx):
         oh = self.tt.FloatTensor(lang_seq.size()[0], len(langIdx2oneHotIdx)).zero_()
-        
+        oh = oh.half() if self.fp16 else oh
         lang_seq = lang_seq.data.tolist()
         for ii, ll in enumerate(lang_seq):
             idx = langIdx2oneHotIdx[ll[0]]
@@ -808,7 +807,7 @@ class NMTmodelRNN(nn.Module):
                 sim_loss = -( (enc_output*enc_output_tgt).sum(dim=2)/(norm1*norm2) ).sum()
             elif self.uni_crit == 'cossim_margin':
                 perm = torch.randperm(len(enc_output))
-                if self.cuda:
+                if self.cuda_:
                     perm = perm.cuda()
                     if self.fp16:
                         perm = perm.half()

train.py
@@ -78,7 +78,7 @@ class MainModel(nn.Module):
         return loss, pred
 
 
-def train_epoch(model, training_data, crit, optimizer, opt, epoch_i, best_BLEU, patience_count, nb_examples_seen, pct_next_save):
+def train_epoch(model, training_data, crit, optimizer, opt, epoch_i, best_BLEU, patience_count, nb_examples_seen, pct_next_save, params):
     ''' Epoch operation in training phase'''
 
     start = time.time()
@@ -136,7 +136,7 @@ def train_epoch(model, training_data, crit, optimizer, opt, epoch_i, best_BLEU, 
             set_grad(params, list(model.parameters()))
             if opt.fp16_scale != 1:
                 for param in params:
-                    param.grad.data = params.grad.data / opt.fp16_scale
+                    param.grad.data = param.grad.data / opt.fp16_scale
             nn.utils.clip_grad_norm(params, 1.0)
         else:
             # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs/LSTMs.
@@ -227,7 +227,7 @@ def eval_epoch(model, validation_data, crit, opt):
 
     return total_loss/n_total_words, n_total_correct/n_total_words
 
-def train(model, training_data, validation_data, crit, optimizer, opt, epoch_i, best_BLEU, patience_count):
+def train(model, training_data, validation_data, crit, optimizer, opt, epoch_i, best_BLEU, patience_count, params):
     ''' Start training '''
 
     nb_examples_seen = 0
@@ -238,7 +238,7 @@ def train(model, training_data, validation_data, crit, optimizer, opt, epoch_i, 
         print('[ Starting epoch', epoch_i+1, ']')
 
         train_loss, train_accu, epoch_i, best_BLEU, patience_count, optimizer, nb_examples_seen, pct_next_save = train_epoch(model, training_data, crit, optimizer, opt,
-                                                                                                            epoch_i, best_BLEU, patience_count, nb_examples_seen, pct_next_save)
+                                                                                                                             epoch_i, best_BLEU, patience_count, nb_examples_seen, pct_next_save, params)
         start = time.time()
         valid_loss, valid_accu = eval_epoch(model, validation_data, crit, opt)
         print('  - (Validation) ppl: {ppl: 8.5f}, accuracy: {accu:3.3f} %, '\
@@ -424,6 +424,14 @@ def nb_lang(lang_data):
     
     return len(lang_token_idx)
 
+def set_grad(params, params_with_grad):
+    for param, param_w_grad in zip(params, params_with_grad):
+        if param.grad is None:
+            param.grad = torch.nn.Parameter(
+                param.data.new().resize_(*param.data.size()))
+        grad = param_w_grad.grad.data
+        param.grad.data.copy_(grad)
+
 def main():
     ''' Main function '''
     parser = argparse.ArgumentParser()
@@ -534,7 +542,6 @@ def main():
         src_lang_insts=(data['train']['src_lang'] if opt.source_lang else None),
         tgt_lang_insts=(data['train']['tgt_lang'] if opt.target_lang else None),
         batch_size=opt.batch_size,
-        fp16= bool(opt.fp16_scale),
         cuda=opt.cuda,
         is_train=True,
         sort_by_length=True)
@@ -548,7 +555,6 @@ def main():
         tgt_lang_insts=(data['valid']['tgt_lang'] if opt.target_lang else None),
         batch_size=opt.valid_batch_size,
         shuffle=False,
-        fp16= bool(opt.fp16_scale),
         cuda=opt.cuda,
         is_train=False,
         sort_by_length=True)
@@ -664,7 +670,7 @@ def main():
     if opt.multi_gpu:
         model = nn.DataParallel(model)
 
-    train(model, training_data, validation_data, crit, optimizer, opt, epoch_i, best_BLEU, patience_count)
+    train(model, training_data, validation_data, crit, optimizer, opt, epoch_i, best_BLEU, patience_count, params)
 
 if __name__ == '__main__':
     main()